6f6190a2f2ae06aa5b2d3c6d97c88350b689b28b
cpettinato <75131453+cpettinato@users.noreply.github.com>
2024-04-16 09:51:53 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hola mundo")
+
+let edad = 1;
+
+edad = edad +1 // edad +=1
+
+console.log(edad)
+
+//+ suma y concatena, pero concatena antes de sumar
+console.log(1+1)
+console.log('1'+1)
+console.log('esteban'+1)
+console.log(1+'1')
+console.log(1*2)
+console.log(10/2)
+console.log(11%2)
+
